1d2e08129c80b15924ac6c8a75e71336d3dbfbdd
DavideFarci <yank92.df@gmail.com>
2023-04-13 15:46:57 +0200

    fix form

--- assets/script.js
+++ assets/script.js
@@ -1,2 +1,6 @@
 const numKM = document.getElementById("numKM").value; 
 const age = document.getElementById("age").value;
+
+const prezzo = (numKM * 0.21);
+const prezzoMinore = (numKM * 0.21) - ((numKM * 0.21) * 20 / 100);
+const prezzoOver = (numKM * 0.21) - ((numKM * 0.21) * 40 / 100);
