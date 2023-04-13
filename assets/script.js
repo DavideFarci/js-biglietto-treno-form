c319717ee247959ed8abc308fe92049ce26fed1d
DavideFarci <yank92.df@gmail.com>
2023-04-13 16:12:51 +0200

    add .addEventListener

--- assets/script.js
+++ assets/script.js
@@ -5,6 +5,19 @@ const prezzo = (numKM * 0.21);
 const prezzoMinore = (numKM * 0.21) - ((numKM * 0.21) * 20 / 100);
 const prezzoOver = (numKM * 0.21) - ((numKM * 0.21) * 40 / 100);
 
+const eleCalc = document.querySelector("calc");
+eleCalc.addEventListener("click", function() {
+    if (age == normal) {
+        document.getElementById("price").innerHTML = prezzo.toFixed(2);
+    }
+    if (age == minorenne) {
+        document.getElementById("price").innerHTML = prezzoMinore.toFixed(2);
+    }
+    if (age == over) {
+        document.getElementById("price").innerHTML = prezzoOver.toFixed(2);
+    }
+})
+
 // if (age == normal) {
 //     document.getElementById("price").innerHTML = prezzo.toFixed(2);
 // }
