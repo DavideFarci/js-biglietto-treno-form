ecc05ceb0f34870d4e674c6f43b6a976acaa9c2b
DavideFarci <yank92.df@gmail.com>
2023-04-13 18:45:24 +0200

    fix table

--- assets/script.js
+++ assets/script.js
@@ -2,7 +2,7 @@
 
 const eleCalc = document.querySelector(".calc");
 eleCalc.addEventListener("click", function() {
-    const nameUser = document.getElementById("name");
+    const nameUser = document.getElementById("name").value;
     const numKM = document.getElementById("numKM").value; 
     const age = document.getElementById("age").value;
 
@@ -10,14 +10,19 @@ eleCalc.addEventListener("click", function() {
     const prezzoMinore = (numKM * 0.21) - ((numKM * 0.21) * 20 / 100);
     const prezzoOver = (numKM * 0.21) - ((numKM * 0.21) * 40 / 100);
 
+    
     if (age == "normal") {
-        document.getElementById("name_pass").innerHTML = nameUser;
+        document.getElementById("tariffa").innerHTML = "Tariffa standard";
         document.getElementById("price").innerHTML = prezzo.toFixed(2);
     }
     if (age == "minorenne") {
+        document.getElementById("tariffa").innerHTML = "Tariffa minori";
         document.getElementById("price").innerHTML = prezzoMinore.toFixed(2);
     }
     if (age == "over") {
+        document.getElementById("tariffa").innerHTML = "Tariffa over 65";
         document.getElementById("price").innerHTML = prezzoOver.toFixed(2);
     }
+
+    document.getElementById("name_pass").innerHTML = nameUser ;
 })
