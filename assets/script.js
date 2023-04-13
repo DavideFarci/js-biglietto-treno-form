f07cbe11ae7e289eaa6b47380a6587d9d650a083
DavideFarci <yank92.df@gmail.com>
2023-04-13 15:58:30 +0200

    add button

--- assets/script.js
+++ assets/script.js
@@ -4,3 +4,13 @@ const age = document.getElementById("age").value;
 const prezzo = (numKM * 0.21);
 const prezzoMinore = (numKM * 0.21) - ((numKM * 0.21) * 20 / 100);
 const prezzoOver = (numKM * 0.21) - ((numKM * 0.21) * 40 / 100);
+
+// if (age == normal) {
+//     document.getElementById("price").innerHTML = prezzo.toFixed(2);
+// }
+// if (age == minorenne) {
+//     document.getElementById("price").innerHTML = prezzoMinore.toFixed(2);
+// }
+// if (age == over) {
+//     document.getElementById("price").innerHTML = prezzoOver.toFixed(2);
+// }
