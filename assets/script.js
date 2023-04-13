c7055ee6e20d0be544224eda0596bbc234045708
DavideFarci <yank92.df@gmail.com>
2023-04-13 17:42:33 +0200

    add bootstrap form

--- assets/script.js
+++ assets/script.js
@@ -1,29 +1,21 @@
-const numKM = document.getElementById("numKM").value; 
-const age = document.getElementById("age").value;
 
-const prezzo = (numKM * 0.21);
-const prezzoMinore = (numKM * 0.21) - ((numKM * 0.21) * 20 / 100);
-const prezzoOver = (numKM * 0.21) - ((numKM * 0.21) * 40 / 100);
 
 const eleCalc = document.querySelector(".calc");
 eleCalc.addEventListener("click", function() {
-    if (age >= 18 && age <= 65) {
+    const numKM = document.getElementById("numKM").value; 
+    const age = document.getElementById("age").value;
+
+    const prezzo = (numKM * 0.21);
+    const prezzoMinore = (numKM * 0.21) - ((numKM * 0.21) * 20 / 100);
+    const prezzoOver = (numKM * 0.21) - ((numKM * 0.21) * 40 / 100);
+
+    if (age == "normal") {
         document.getElementById("price").innerHTML = prezzo.toFixed(2);
     }
-    if (age < 18) {
+    if (age == "minorenne") {
         document.getElementById("price").innerHTML = prezzoMinore.toFixed(2);
     }
-    if (age > 65) {
+    if (age == "over") {
         document.getElementById("price").innerHTML = prezzoOver.toFixed(2);
     }
 })
-
-// if (age == normal) {
-//     document.getElementById("price").innerHTML = prezzo.toFixed(2);
-// }
-// if (age == minorenne) {
-//     document.getElementById("price").innerHTML = prezzoMinore.toFixed(2);
-// }
-// if (age == over) {
-//     document.getElementById("price").innerHTML = prezzoOver.toFixed(2);
-// }
